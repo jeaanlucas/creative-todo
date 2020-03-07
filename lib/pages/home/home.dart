fc83d09faee37c36b20682c8a953e7ed8df7bb96
Jean Lucas <jeanl.camillo@gmail.com>
2020-03-06 22:20:47 -0300

    Criado o card para add uma tarefa

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,5 +1,7 @@
+import 'package:creativetodo/widgets/empty_todo_card.dart';
 import 'package:creativetodo/widgets/gradient_background.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -32,12 +34,75 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     );
   }
 
+  String get diaAtual => DateFormat('EEEE').format(DateTime.now());
+
+  String get mesAtual => DateFormat('MMM').format(DateTime.now());
+
+  String get dataAtual => DateFormat('d').format(DateTime.now());
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double width = size.width;
+    final double heigth = size.height;
+
     return GradientBackground(
       color: Colors.blueAccent,
       child: Scaffold(
         backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            'Creative TODO',
+          ),
+          centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                top: .0,
+                left: width * .1,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: Text(
+                      diaAtual,
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '$dataAtual - $mesAtual',
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: PageView.builder(
+                itemCount: 1,
+                controller: _pageController,
+                itemBuilder: (BuildContext _, int index) => EmptyTodoCard(),
+              ),
+            ),
+            SizedBox(
+              height: heigth * .05,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/empty_todo_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class EmptyTodoCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+        16.0,
+      )),
+      elevation: 6.0,
+      margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(
+          16.0,
+        ),
+        color: Colors.white,
+        child: InkWell(
+          onTap: () => {},
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: 16.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add,
+                  size: 55.0,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  'Add Item',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
